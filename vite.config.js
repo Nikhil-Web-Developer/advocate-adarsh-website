01abe834711b6d15c8972b8c81b4bc967b85d349
Nikhil Hans <nikhilhans1851@gmail.com>
2026-08-29 17:08:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: false
+  base: '/advocate-adarsh-website/',
+  build: {
+    outDir: 'dist'
   }
-});
+})
